3f6143a35ef645dc3aad5f15bcfcc16b55cdcba4
wgeethkinduranga-svg <w.geethkinduranga@gmail.com>
2026-08-24 12:04:20 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,49 @@
 const CACHE_NAME = "finance-dashboard-v1";
 
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
 self.addEventListener("install", event => {
-    console.log("Finance Dashboard SW installed");
-    self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-    console.log("Finance Dashboard SW activated");
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
 
-    event.waitUntil(
-        self.clients.claim()
-    );
+  self.clients.claim();
 });
 
-/*
- Do NOT intercept Firebase, Firestore, authentication,
- Chart.js, XLSX or other external requests.
-*/
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, copy);
+        });
+
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
